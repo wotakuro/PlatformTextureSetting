2d3eb7299c4437511f5d8650ade4c55a4793c4dd
wotakuro <wotakuro@gmail.com>
2016-04-06 12:58:28 +0900

    Add platform

---
+++ Assets/PlatformTextureSetting/Editor/Platforms/TextureImportSettingN3DS.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PlatformTextureSetting
+{
+	public class TextureImportSettingN3DS : TextureImportSettingBase
+	{
+		protected override string targetPlatform
+		{
+			get { return "Nintendo 3DS"; }
+		}
+		[MenuItem("Tools/PlatformTextureSetting/Nintendo3DS")]
+		public static void ShowWindow()
+		{
+			var window = EditorWindow.GetWindow(typeof(TextureImportSettingN3DS));
+		}
+	}
+}
